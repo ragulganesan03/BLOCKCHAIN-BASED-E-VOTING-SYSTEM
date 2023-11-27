e0aaeefcdc5bb57d47a90eebf30c1d4615967df3
ragulganesan03 <130173143+ragulganesan03@users.noreply.github.com>
2023-11-27 18:54:22 +0530

    Add files via upload
    Initial Commit without react-app folder

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+async function main() {
+  const Voting = await ethers.getContractFactory("Voting");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const Voting_ = await Voting.deploy(["Ragul", "Dhanush", "Divya", "Sairam", "Nihal"],2);
+  console.log("Contract address:", Voting_.address);
+}
+
+main()
+ .then(() => process.exit(0))
+ .catch(error => {
+   console.error(error);
+   process.exit(1);
+ });
